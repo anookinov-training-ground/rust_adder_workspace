07167e3e45f5e5fad482195ea6b418019b09a73f
Alexander Nookinov <anookinov@gmail.com>
2021-08-28 12:18:32 +0700

    add add_two package

--- add-one/src/lib.rs
+++ add-one/src/lib.rs
@@ -1,11 +1,17 @@
+// cargo publish
+// use rand;
+
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
 
+// adder_workspace % cargo test -p add-one
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(3, add_one(2));
     }
 }
--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,10 +1,18 @@
 use add_one;
+// use rand;
 
+// adder_workspace % cargo run -p adder
 fn main() {
     let num = 10;
     println!(
         "Hello, world! {} plus one is {}!",
         num,
         add_one::add_one(num)
+    );
+
+    println!(
+        "Hello, world! {} plus two is {}!",
+        num,
+        add_two::add_two(num)
     );
 }
